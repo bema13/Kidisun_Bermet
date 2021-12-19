5fca067ab8a9b460d0892f13ee4117fee3380815
bema13 <kurmanbekova_b@auca.kg>
2021-12-20 03:12:52 +0600

    commit bema

---
+++ app/src/main/java/com/example/kidisun1/kidisun5.kt
@@ -0,0 +1,11 @@
+package com.example.kidisun1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class kidisun5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kidisun5)
+    }
+}
